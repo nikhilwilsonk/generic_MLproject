7d74198eb6f6898aa72d1aafacf0c6b0990619f5
nikhilss219 <nikhil.wilsonk@gmail.com>
2024-12-30 09:14:49 +0530

    added test cases in exception and logger files

--- src/execption.py
+++ src/execption.py
@@ -1,5 +1,5 @@
 import sys
-
+import logging
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() #we are only interested in the third variable
     #on which file and which line number the exception has occurred
@@ -19,4 +19,9 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-    
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Logging has started")
+        raise CustomException(e,sys)

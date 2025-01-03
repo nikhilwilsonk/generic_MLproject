e73290a78fbe7c67ce5048448daa2ebe0a145aa9
nikhilss219 <nikhil.wilsonk@gmail.com>
2025-01-03 10:33:39 +0530

    Completed data transformation function

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -15,9 +17,20 @@ class DataIngestionConfig:
 
 class DataIngestion:
     def __init__(self):
+        '''
+        Creating the path for the raw, train and test data
+        '''
         self.ingestion_config=DataIngestionConfig()
     
     def initiate_data_ingestion(self):
+        '''
+        Recieving file paths from self,
+        Reading the source dataset to work with;
+        Creating the artifacts folder;
+        saving the raw file;
+        Train test split and saving the files in respective paths;
+        RETURN: train data path and test data path
+        '''
         logging.info("Entered the data ingestion method or component")
         try:
             df=pd.read_csv("/Users/nikhilwilson/Documents/tutorials/ML_portfolio/proj_1/notebook/data/stud.csv")            
@@ -40,7 +53,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
-
-
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transform(train_data,test_data)
